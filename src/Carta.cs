aaf82f24cc87d6d8a64abad35eae2512b64f0209
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-08-11 19:06:36 -0300

    nuevo campo de jerarquiaTruco en la clase carta y método para calcular la jerarquia

--- src/Carta.cs
+++ src/Carta.cs
@@ -1,6 +1,6 @@
 namespace EspacioCarta
 {
-    public enum palos{
+    public enum Palos{
         Oro,
         Copa,
         Espada,
@@ -9,14 +9,16 @@ namespace EspacioCarta
     public class Carta
     {
         private string nombre;
-        private palos palo;
+        private Palos palo;
         private int numero;
+        private int jerarquiaTruco;
 
         public string Nombre => nombre;
-        public palos Palo => palo;
+        public Palos Palo => palo;
         public int Numero => numero;
+        public int JerarquiaTruco => jerarquiaTruco;
 
-        public Carta(palos palo, int numero){
+        public Carta(Palos palo, int numero){
             this.palo = palo;
             this.numero = numero;
             string NuevoNombre = numero switch
@@ -26,7 +28,28 @@ namespace EspacioCarta
                 12 => "Rey de",
                 _ => numero.ToString() + " de"
             };
-            nombre = $"{NuevoNombre} de {palo}";
+            nombre = $"{NuevoNombre} {palo}";
+            jerarquiaTruco = CalcularJerarquia(palo, numero);
+        }
+        private int CalcularJerarquia(Palos palo, int numero){
+            if (palo == Palos.Espada && numero == 1) return 14;
+            if (palo == Palos.Basto && numero == 1) return 13;
+            if (palo == Palos.Espada && numero == 7) return 12;
+            if (palo == Palos.Oro && numero == 7) return 11;
+            return numero switch
+            {
+                3 => 10,
+                2 => 9,
+                1 => 8,
+                12 => 7,
+                11 => 6,
+                10 => 5,
+                7 => 4,
+                6 => 3,
+                5 => 2,
+                4 => 1,
+                _ => 0
+            };
         }
     }
 }
