6972bba8082250cb679e969dd1de6bbd08fa1d6d
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-08-14 15:55:21 -0300

    prueba de la clase Flor y secuenciaFlor exitosa

--- src/Program.cs
+++ src/Program.cs
@@ -2,84 +2,75 @@
 using EspacioJugador;
 using EspacioMazo;
 using EspacioEnvido;
+using EspacioFlor;
 
 class Program{
     static void Main()
     {
-        // instancias de jugadores y mazo
-        Mazo mazo = new Mazo();
+        List<Carta> maso = new List<Carta>(3);
+        for (int i = 0; i < 3; i++)
+        {
+            maso.Add(new Carta(Palos.Oro,i));
+        }
         Jugador jugador1 = new Jugador("Jugador1");
-        Jugador jugador2 = new Jugador("Jugador2");
-
-        // mezclado de las cartas
-        mazo.Barajar();
 
         // repartir las cartas
-        jugador1.RecibirCartas(mazo.Repartir(3));
-        jugador2.RecibirCartas(mazo.Repartir(3));
+        jugador1.RecibirCartas(maso);
 
         // mostrar la mano de cada jugador
         Console.WriteLine($"{jugador1.Nombre}");
         foreach (var naipe in jugador1.Mano){
             Console.WriteLine($"- {naipe.Nombre}");
         }
-        
-        Console.WriteLine($"{jugador2.Nombre}");
-        foreach (var naipe in jugador2.Mano){
-            Console.WriteLine($"- {naipe.Nombre}");
-        }
-
-        Console.WriteLine($"Cantidad de cartas en Mazo: {mazo.Naipes.Count}");
 
         jugador1.CalcularEnvido();
         jugador1.CalcularFlor();
-        jugador2.CalcularEnvido();
-        jugador2.CalcularFlor();
 
         Console.WriteLine($"Puntos del envido de Jugador1: {jugador1.PuntosEnvido}");
-        Console.WriteLine($"Puntos del envido de Jugador2: {jugador2.PuntosEnvido}");
         Console.WriteLine($"Puntos de flor de Jugador1: {jugador1.PuntosFlor}");
-        Console.WriteLine($"Puntos de flor de Jugador2: {jugador2.PuntosFlor}");
-        
-        int opc = 0;
-        SecuenciaEnvido secuencia = new SecuenciaEnvido();
+        int opc;
+        SecuenciaFlor secuencia = new SecuenciaFlor();
+
         do
         {
             Console.WriteLine("¿Qué desea hacer?");
-            Console.WriteLine("1. Jugar Envido");
-            Console.WriteLine("2. Jugar RealEnvido");
-            Console.WriteLine("3. Jugar FaltaEnvido");
-            Console.WriteLine("4. Salir");
+            Console.WriteLine("1. Jugar Flor");
+            Console.WriteLine("2. Jugar ContraFlor");
+            Console.WriteLine("3. Jugar ContraFlorResto");
+            Console.WriteLine("0. Salir");
             string opcion = Console.ReadLine();
             opc = int.Parse(opcion);
 
             switch (opc)
             {
                 case 1:
-                    CantoEnvido Canto = new CantoEnvido(TipoEnvido.Envido, jugador1.Nombre, 14);
+                    CantoFlor Canto = new CantoFlor(TipoFlor.Flor, jugador1.Nombre);
                     secuencia.AgregarCanto(Canto);
-                    Console.WriteLine("Envido!!!");
+                    Console.WriteLine("FLOR!!!");
                     break;
                 case 2:
-                    CantoEnvido Canto1 = new CantoEnvido(TipoEnvido.RealEnvido, jugador1.Nombre, 14);
+                    CantoFlor Canto1 = new CantoFlor(TipoFlor.ContraFlor, jugador1.Nombre);
                     secuencia.AgregarCanto(Canto1);
-                    Console.WriteLine("RealEnvido!!!");
+                    Console.WriteLine("CONTRAFLOR!!!");
                     break;
                 case 3:
-                    CantoEnvido Canto2 = new CantoEnvido(TipoEnvido.FaltaEnvido, jugador1.Nombre, 14);
+                    CantoFlor Canto2 = new CantoFlor(TipoFlor.ContraFlorResto, jugador1.Nombre);
                     secuencia.AgregarCanto(Canto2);
-                    Console.WriteLine("FaltaEnvido!!!");
+                    Console.WriteLine("CONTRA FLOR AL RESTO!!!");
                     break;
-                
+
                 default:
                     Console.WriteLine("Opción inválida");
                     break;
             }
         } while (opc != 0);
-
+        foreach (var juego in secuencia.Jugada)
+        {
+            Console.WriteLine($"Jugador: {juego.Jugador} dice {juego.Tipo} que vale {juego.Puntos}");
+        }
         secuencia.CalcularPuntos();
-        Console.WriteLine($"Puntos de la jugada: {secuencia.PuntosJugada}");
-        secuencia.Jugada.ForEach(canto => Console.WriteLine($"{canto.Jugador} ha dicho {canto.Tipo} y ha ganado {canto.Puntos} puntos"));
+        Console.WriteLine($"Valor de la jugada: {secuencia.PuntosJugada}");
+
 
 
     }
