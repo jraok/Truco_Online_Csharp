09cf3842b7e7d3fbcef45bbb9010102e887b9991
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-08-15 23:16:20 -0300

    cambio en el campo Naipes de la clase Mazo, son mejoras pensando en la serialización del objeto

--- src/ElementosBasicos/Mazo.cs
+++ src/ElementosBasicos/Mazo.cs
@@ -2,8 +2,9 @@ using EspacioCarta;
 namespace EspacioMazo
 {
     public class Mazo{
-        private List<Carta> naipes = new List<Carta>();
-        public List<Carta> Naipes => naipes;
+        private static readonly Random rnd = new Random();
+        public List<Carta> Naipes { get; private set; } = new List<Carta>();
+
         public Mazo(){
             for (int i = 0; i < 4; i++)
             {
@@ -11,19 +12,18 @@ namespace EspacioMazo
                 {
                     if (j != 8 && j != 9)
                     {
-                        naipes.Add(new Carta((Palos)i, j));
+                        Naipes.Add(new Carta((Palos)i, j));
                     }
                 }
             }
         }
         public void Barajar(){
-            Random rnd = new Random();
-            naipes = naipes.OrderBy(x => rnd.Next()).ToList();
+            Naipes = Naipes.OrderBy(x => rnd.Next()).ToList();
         }
 
         public List<Carta> Repartir(int cantidad){
-            List<Carta> mano = naipes.Take(cantidad).ToList();
-            naipes.RemoveRange(0,cantidad);
+            List<Carta> mano = Naipes.Take(cantidad).ToList();
+            Naipes.RemoveRange(0,cantidad);
             return mano;
         }
         
