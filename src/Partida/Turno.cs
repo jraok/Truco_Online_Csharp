0c7241c0d8b65f15b1381001bfcb112e747b97fb
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-08-16 17:29:11 -0300

    nueva clase turno y carpeta en mi directorio

---
+++ src/Partida/Turno.cs
@@ -0,0 +1,18 @@
+using System.Text.Json.Serialization;
+using EspacioJugador;
+using EspacioCarta;
+
+namespace EspacioTurno
+{
+    public class Turno{
+        private string jugador;
+        private Carta cartaJugada;
+        public string Jugador => jugador;
+        public Carta CartaJugada => cartaJugada;
+        [JsonConstructor]
+        public Turno(string jugador, Carta carta){
+            this.jugador = jugador;
+            this.cartaJugada = carta;
+        }
+    }
+}
