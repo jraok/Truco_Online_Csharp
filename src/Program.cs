34bb965932325bc9621cab24de09600356a357e8
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-08-17 19:31:27 -0300

    clase Ronda: prueba exitosa

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,4 @@
-﻿using EspacioEnvido;
-using EspacioFlor;
-using EspacioTruco;
-using EspacioJugador;
+﻿using EspacioJugador;
 using EspacioMazo;
 using EspacioRonda;
 using EspacioTurno;
@@ -15,6 +12,7 @@ class Program{
         mazo.Barajar();
         Player1.RecibirCartas(mazo.Repartir(3));
         Player2.RecibirCartas(mazo.Repartir(3));
+
         foreach (var carta in Player1.Mano)
         {
             Console.WriteLine($"{Player1.Nombre} - {carta.Nombre}");
@@ -28,43 +26,13 @@ class Program{
 
         for (int i = 0; i < 3; i++)
         {
-            Ronda Ronda = new(i+1);
-            Ronda.AgregarTurno(new Turno(Player1.Nombre, Player1.JugarCarta(0)));
-            Ronda.AgregarTurno(new Turno(Player1.Nombre, Player2.JugarCarta(0)));
+            Ronda Ronda = new(i);
+            Ronda.AgregarTurno(new Turno(Player1.Nombre,Player1.JugarCarta(0)));
+            Ronda.AgregarTurno(new Turno(Player2.Nombre,Player2.JugarCarta(0)));
             Rondas.Add(Ronda);
+            Console.WriteLine($"Ganador de la ronda: {i+1} ---> jugador = {Ronda.DeterminarGanador().ToString()}");
         }
-        foreach (var Ronda in Rondas)
-        {
-            Ronda.AgregarCanto(new CantoEnvido(TipoEnvido.Envido, Player1.Nombre, 10));
-            Ronda.AgregarCanto(new CantoTruco(TipoTruco.Truco, Player2.Nombre));
-            Ronda.AgregarCanto(new CantoFlor(TipoFlor.Flor, Player1.Nombre));
-            Ronda.AsignarPuntos(11,12);
-        }
-        foreach (var Ronda in Rondas)
-        {
-            Console.WriteLine("------------------------");
-            Console.WriteLine($"Ronda: {Ronda.Numero}");
-            foreach (var turno in Ronda.Turnos)
-            {
-                Console.WriteLine($"{turno.Jugador} - {turno.CartaJugada.Nombre}");
-            }
-            Console.WriteLine("Secuencia envido");
-            foreach (var envido in Ronda.SecEnvido.Jugada)
-            {
-                Console.WriteLine($"{envido.Jugador} - {envido.Tipo} - {envido.Puntos}");
-            }
-            Console.WriteLine("Secuencia truco");
+       
 
-            foreach (var truco in Ronda.SecTruco.Jugada)
-            {
-                Console.WriteLine($"{truco.Jugador} - {truco.Tipo} - {truco.Puntos}");
-            }
-            Console.WriteLine("Secuencia flor");
-
-            foreach (var flor in Ronda.SecFlor.Jugada)
-            {
-                Console.WriteLine($"{flor.Jugador} - {flor.Tipo} - {flor.Puntos}");
-            }
-        }
     }
 }
