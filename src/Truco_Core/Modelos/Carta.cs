bfc0331ea8a5f78dae67262ed60d2133c5dc27af
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-12-12 01:14:03 -0300

    delegue el calculo de la jerarquia de la clase carta a la calculadora

--- src/Truco_Core/Modelos/Carta.cs
+++ src/Truco_Core/Modelos/Carta.cs
@@ -12,38 +12,17 @@ namespace Truco.Core.Modelos
         private int numero;
         public string Nombre => numero switch
             {
-              10 => $"Sota de {palo}",
-              11 => $"Caballo de {palo}",
-              12 => $"Rey de {palo}",
-              _  => $"{numero} de {palo}"
+            10 => $"Sota de {palo}",
+            11 => $"Caballo de {palo}",
+            12 => $"Rey de {palo}",
+            _  => $"{numero} de {palo}"
         };
         public Palos Palo => palo;
         public int Numero => numero;
-        public int JerarquiaTruco => CalcularJerarquia(palo, numero);
 
         public Carta(Palos palo, int numero){
             this.palo = palo;
             this.numero = numero;
-        }
-        private int CalcularJerarquia(Palos palo, int numero){
-            if (palo == Palos.Espada && numero == 1) return 14;
-            if (palo == Palos.Basto && numero == 1) return 13;
-            if (palo == Palos.Espada && numero == 7) return 12;
-            if (palo == Palos.Oro && numero == 7) return 11;
-            return numero switch
-            {
-                3 => 10,
-                2 => 9,
-                1 => 8,
-                12 => 7,
-                11 => 6,
-                10 => 5,
-                7 => 4,
-                6 => 3,
-                5 => 2,
-                4 => 1,
-                _ => 0
-            };
         }
     }
 }
--- src/Truco_Core/Reglas/Calculadora.cs
+++ src/Truco_Core/Reglas/Calculadora.cs
@@ -1,7 +1,7 @@
 using Truco.Core.Modelos;
 namespace Truco.Core.Reglas
 {
-    public static class Calculadora{
+    public static class CalculadoraTruco{
         private const int BonusEnvido = 20;
         private static int ValorEnvido(Carta carta){
             if (carta.Numero >= 10) return 0;
@@ -27,6 +27,26 @@ namespace Truco.Core.Reglas
             if (cartas[0].Palo == cartas[1].Palo && cartas[1].Palo == cartas[2].Palo)
             return (cartas.Sum(carta => ValorEnvido(carta)) + BonusEnvido);
             else return 0;
+        }
+        private static int JerarquiaCarta(Carta carta){
+            if (carta.Palo == Palos.Espada && carta.Numero == 1) return 14;
+            if (carta.Palo == Palos.Basto && carta.Numero == 1) return 13;
+            if (carta.Palo == Palos.Espada && carta.Numero == 7) return 12;
+            if (carta.Palo == Palos.Oro && carta.Numero == 7) return 11;
+            return carta.Numero switch
+            {
+                3 => 10,
+                2 => 9,
+                1 => 8,
+                12 => 7,
+                11 => 6,
+                10 => 5,
+                7 => 4,
+                6 => 3,
+                5 => 2,
+                4 => 1,
+                _ => 0
+            };
         }
     }
 }
