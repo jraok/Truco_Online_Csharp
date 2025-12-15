53af8046bc6543ad795a1e4fbf9e13dc8b0a7f21
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-12-15 01:16:27 -0300

    separé el archivo de acciones en uno por cada accion necesaria

--- src/Truco_App/Acciones/JugarCarta.cs
+++ src/Truco_App/Acciones/JugarCarta.cs
@@ -1,32 +1,9 @@
-using Truco.App;
-using Truco.Core.Juego;
 using Truco.Core.Modelos;
+using Truco.Core.Juego;
+using Truco.Core.Reglas;
 
 namespace Truco.App.Acciones
 {
-    public static class IniciarMano
-    {
-        public static void Ejecutar(Partida partida)
-        {
-            var JMano = partida.JugadorMano;
-            var JPie = partida.JugadorPie;
-            if (JMano.Puntaje >= partida.PuntosPartida || JPie.Puntaje >= partida.PuntosPartida) throw new InvalidOperationException("La partida terminó");
-            
-            var mazo = new Mazo();
-            mazo.Barajar();
-            partida.SumarMano();
-
-            JMano.LimpiarCartas();
-            JPie.LimpiarCartas();
-            JMano.RecibirCartas(mazo.Repartir(3));
-            JPie.RecibirCartas(mazo.Repartir(3));
-
-            partida.AsignarMano(new Mano(JMano, JPie));
-            partida.ManoActual.IniciarSiguienteRonda();
-
-            partida.AsignarTurno(JMano);
-        }
-    }
     public static class JugarCarta
     {
         private static void ResolverGanadorRonda(Partida partida)
