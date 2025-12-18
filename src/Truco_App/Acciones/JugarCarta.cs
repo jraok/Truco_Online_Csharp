617f82f86c2fe195cb3691691df7e51f6a33b3f6
Joel Román Arancibia <joelromanarancibia@gmail.com>
2025-12-18 16:45:02 -0300

    nueva condicion para cantar los juegos

--- src/Truco_App/Acciones/JugarCarta.cs
+++ src/Truco_App/Acciones/JugarCarta.cs
@@ -22,8 +22,10 @@ namespace Truco.App.Acciones
         }
         public static void Ejecutar(Partida partida, string nombreJugador, int indiceCarta)
         {
-            if (partida.ManoActual == null)throw new InvalidOperationException("No hay mano en juego");
-            if (nombreJugador != partida.TurnoActual.Nombre) throw new InvalidOperationException($"No es el turno de {nombreJugador}");
+            if (partida.ManoActual == null && !partida.ManoActual.Finalizada)
+                throw new InvalidOperationException("No hay mano en juego");
+            if (nombreJugador != partida.TurnoActual.Nombre) 
+                throw new InvalidOperationException($"No es el turno de {nombreJugador}");
 
             var carta = partida.TurnoActual.TirarCarta(indiceCarta);
             partida.ManoActual.RondaActual!.AgregarTurno(new Turno(partida.TurnoActual.Nombre, carta));
